46e09d8be1e64d871896d37613c9b8e95ffe052e
Fedor <fedor@gmail.com>
2025-08-05 18:34:22 +0300

    Там переместил чуток и нормально

--- Web/app.py
+++ Web/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template, redirect, url_for, flash
 from database import register_user, authenticate_user
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = ''
 
 
 @app.route("/")
